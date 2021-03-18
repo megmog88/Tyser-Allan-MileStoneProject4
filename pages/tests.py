9aef74bf47e1807896aec89f43f0c5cd6de9c8c7
megmog88 <meghanspence88@hotmail.co.uk>
2021-03-18 15:20:29 +0000

    adding test pages and test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_draping_page_status_code(self):
+        response = self.client.get('/draping/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_landscaping_page_status_code(self):
+        response = self.client.get('/landscaping/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_merchandise_status_code(self):
+        response = self.client.get('/merchandise/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_contact_status_code(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
