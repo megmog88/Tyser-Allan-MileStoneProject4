d7e9af1408fee8e77ba41d7d851ca5ea703750ec
megmog88 <meghanspence88@hotmail.co.uk>
2021-03-23 21:24:32 +0000

    adding css to style project to look secy

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from .views import HomePageView, DrapingPageView, LandscapingPageView, ContactPageView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('draping/', DrapingPageView.as_view(), name='draping'),
@@ -7,4 +10,4 @@ urlpatterns = [
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('', HomePageView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- tyserallan/settings.py
+++ tyserallan/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
+STATIC_ROOT = "/var/www/example.com/static/"
+
 #
 
 AUTHENTICATION_BACKENDS = (
