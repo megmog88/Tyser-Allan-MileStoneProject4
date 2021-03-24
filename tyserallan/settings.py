94d9f67821ecfa24ba37d95bbd2b79f882cdd4e5
megmog88 <meghanspence88@hotmail.co.uk>
2021-03-24 09:45:26 +0000

    added requirements txt

--- tyserallan/settings.py
+++ tyserallan/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +82,13 @@ WSGI_APPLICATION = 'tyserallan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
 DATABASES = {
-    'default': dj_database_url.parse('postgres://lmcnzlukzozfhv:2203181cd6f687b7b1a18b460cd9050783c00d1a8484941b20ad3c6af9920de4@ec2-34-247-118-233.eu-west-1.compute.amazonaws.com:5432/d2vlsqkv0drf29')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -148,3 +150,6 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
+
+
+# Configure Django App for Heroku.
