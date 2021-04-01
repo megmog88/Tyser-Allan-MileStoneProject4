4a20c2e9dd2abcf6976b85c1307c7784e93dd1c3
megmog88 <meghanspence88@hotmail.co.uk>
2021-04-01 17:30:30 +0000

    starting to put content into draping page

--- tyserallan/settings.py
+++ tyserallan/settings.py
@@ -157,20 +157,17 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 
+
 # gmail_send/settings.py
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-if 'DEVELOPMENT' in env:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'tyserallan@gmail.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
 
 # Google ReCaptcha
 
