6e6b00b6a577db2238e193b7306fbac2a77191b8
megmog88 <meghanspence88@hotmail.co.uk>
2021-04-22 11:24:26 +0000

    change navbar background color

--- draping/urls.py
+++ draping/urls.py
@@ -3,5 +3,5 @@ from .views import DrapingListView, DrapingDetailView
 
 urlpatterns = [
     path('draping', DrapingListView.as_view(), name='draping'),
-    path('object/<int:pk>/', DrapingDetailView.as_view(), name='drapinginfo'),
+    path('drapes/<int:pk>/', DrapingDetailView.as_view(), name='drapinginfo'),
 ]
