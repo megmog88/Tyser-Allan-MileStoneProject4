cdfd29e4f46cea5e641be60c4dbabae8f4c3494c
megmog88 <meghanspence88@hotmail.co.uk>
2021-05-05 13:21:21 +0000

    added size option for bag on structures

--- merchandise/models.py
+++ merchandise/models.py
@@ -7,6 +7,7 @@ class Merchandise(models.Model):
     title = models.CharField(max_length=200)
     cover = models.ImageField('media/images/')
     description = models.TextField()
+    different_sizes = models.BooleanField(default=False, null=False, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
