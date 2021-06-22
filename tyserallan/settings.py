0fc5c17fbf26129e9f130e3d4285d6da8595399d
megmog88 <meghanspence88@hotmail.co.uk>
2021-06-22 10:30:30 +0000

    final commit

--- tyserallan/settings.py
+++ tyserallan/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = False
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
